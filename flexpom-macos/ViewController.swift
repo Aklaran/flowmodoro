6b1d5c0d7c1df5ebdcd6ee9befd6427322c77523
Bo Tembunkiart <botbunk@gmail.com>
2019-12-13 14:47:52 -0500

    Initial Commit

---
+++ flexpom-macos/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  flexpom-macos
+//
+//  Created by Siri  Tembunkiart on 12/13/19.
+//  Copyright © 2019 stembunk. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
