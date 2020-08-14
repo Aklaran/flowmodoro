412144454e51640f76c3df957180dcd5a5f9fbb3
Aklaran <botbunk@gmail.com>
2020-08-13 23:58:56 -0700

    Gets something of a timer going...

--- flowmodoro-js/src/components/js/App.js
+++ flowmodoro-js/src/components/js/App.js
@@ -1,23 +1,53 @@
-import React from 'react';
-import logo from '../../assets/img/logo.svg';
+import React, { useEffect, useState } from 'react';
 import '../css/App.css';
 
 function App() {
+  const pomodoroDurationSec = 25;
+  const shortBreakTimeSec = 5;
+  const breakRatio = Math.ceil(pomodoroDurationSec / shortBreakTimeSec);
+  const longBreakTimeSec = 10 * 60;
+
+  const [focusTimeSec, setFocusTime] = useState(pomodoroDurationSec);
+  const [breakTimeSec, setBreakTime] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+
+  function toggle() {
+    setIsActive(!isActive);
+  }
+
+  function reset() {
+    setFocusTime(pomodoroDurationSec);
+    setBreakTime(0);
+    setIsActive(false);
+  }
+
+  useEffect(() => {
+    let interval = null;
+    if (isActive) {
+      interval = setInterval(() => {
+        setFocusTime(seconds => seconds - 1);
+        if (focusTimeSec % breakRatio === 1) {
+          setBreakTime(seconds => seconds + 1);
+        }
+      }, 1000);
+    } else if (!isActive && breakTimeSec !== 0) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [isActive, focusTimeSec, breakTimeSec, breakRatio]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Wtf is Javascript even...</h1>
+        <span>Focus: {focusTimeSec}s</span>
+        <span>Break: {breakTimeSec}s</span>
+        <button className={`button button-primary button-primary-${isActive ? 'active' : 'inactive'}`} onClick={toggle}>
+          {isActive ? 'Pause' : 'Start'}
+        </button>
+        <button className="button" onClick={reset}>
+          Reset
+        </button>
       </header>
     </div>
   );
