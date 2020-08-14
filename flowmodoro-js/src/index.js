93ed620dde71c149287c6e8a0e731953c2074cd1
Aklaran <botbunk@gmail.com>
2020-08-13 22:21:02 -0700

    Organizes files

--- flowmodoro-js/src/index.js
+++ flowmodoro-js/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './components/css/index.css';
+import App from './components/js/App';
+import * as serviceWorker from './models/serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
