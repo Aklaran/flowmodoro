485336de2b8f37dcfd11794b062be503682e64f4
Aklaran <botbunk@gmail.com>
2020-08-14 00:52:47 -0700

    Updates

---
+++ precache-manifest.3cde081e3f46edb44814d24c8135df6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d12860b29b3ea2cbe09b558b840788d",
+    "url": "/flowmodoro/index.html"
+  },
+  {
+    "revision": "edda643006dfb3da4553",
+    "url": "/flowmodoro/static/css/main.1bde8307.chunk.css"
+  },
+  {
+    "revision": "fb0a8cd96884bc828939",
+    "url": "/flowmodoro/static/js/2.f9b14ff7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/flowmodoro/static/js/2.f9b14ff7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edda643006dfb3da4553",
+    "url": "/flowmodoro/static/js/main.01bdcbf2.chunk.js"
+  },
+  {
+    "revision": "163b43ce039a86ef800d",
+    "url": "/flowmodoro/static/js/runtime-main.7d1b7ea0.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/flowmodoro/precache-manifest.b1f0f2be56230cd904cd5322a7a70e5a.js"
+  "/flowmodoro/precache-manifest.3cde081e3f46edb44814d24c8135df6e.js"
 );
 
 self.addEventListener('message', (event) => {
