aacffbf56dfe580a08eee8edf9e0eed6d7a236b1
Aklaran <botbunk@gmail.com>
2020-08-13 22:44:36 -0700

    Updates

---
+++ precache-manifest.bf19b2a9690826ecb8c08b0675b44ab6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6d485d26d314cdfc0b28e134ab88eb3",
+    "url": "/flowmodoro/index.html"
+  },
+  {
+    "revision": "9391484812fa5e5daab0",
+    "url": "/flowmodoro/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e134c696f4c3670279a3",
+    "url": "/flowmodoro/static/js/2.543c8d33.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/flowmodoro/static/js/2.543c8d33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9391484812fa5e5daab0",
+    "url": "/flowmodoro/static/js/main.f3897679.chunk.js"
+  },
+  {
+    "revision": "163b43ce039a86ef800d",
+    "url": "/flowmodoro/static/js/runtime-main.7d1b7ea0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/flowmodoro/static/media/logo.5d5d9eef.svg"
+  }
+]);
