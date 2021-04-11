9ef916475a15d58f954b38d5f1d2e06f5a0b0b3d
Aklaran <botbunk@gmail.com>
2021-04-10 21:18:04 -0700

    Adds background image

--- flowmodoro-js/src/components/App.js
+++ flowmodoro-js/src/components/App.js
@@ -3,16 +3,40 @@ import styled from "styled-components";
 
 import Timer from "./Timer";
 import { COLORS } from "../constants";
+import backgroundImage from "../assets/images/the_glacier_above.jpg";
 
 export default function App() {
     return (
         <Wrapper>
             <Timer />
+            <Background />
+            <BackgroundOverlay />
         </Wrapper>
     );
 }
 
 const Wrapper = styled.div`
     height: 100%;
-    background-color: ${COLORS.gray300};
+`;
+
+const Background = styled.div`
+    position: absolute;
+    background-image: url(${backgroundImage});
+    background-size: cover;
+    background-position: center center;
+    height: 100%;
+    width: 100%;
+    top: 0;
+    left: 0;
+    z-index: -10;
+`;
+
+const BackgroundOverlay = styled.div`
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    background-color: rgba(255, 255, 255, 0.9);
+    z-index: -9;
 `;
